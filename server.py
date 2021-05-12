877adf01c47f5bf2020f90b8dfdc9562f5b1d73e
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-05-12 14:26:00 +0200

    run app in background

--- server.py
+++ server.py
@@ -1,9 +1,19 @@
 from flask import Flask,render_template
+import multiprocessing
+from time import sleep
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template("index.html")
 
+def runApp():
+	print('Running app in background')
+	app.run(port=8080)
+
 if __name__ == '__main__':
-	app.run(port=8080, debug=True)
+	p=multiprocessing.Process(target=runApp)
+	p.start()
+sleep(2)
+print('test')
