aac6a801875a3afcf3d1dc661adff185dc7d95de
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-05-13 13:46:38 +0200

    kill wifi processes and restart atexit

--- server.py
+++ server.py
@@ -1,26 +1,45 @@
 from flask import Flask,render_template
-from time import sleep
-import os,multiprocessing,subprocess
 from dotenv import load_dotenv
+import os,multiprocessing,subprocess,atexit
+
 
 if os.geteuid() != 0:
 	print("This script requires sudo privs!")
 	exit()
 
+
+#kill all wifi processes
+os.system("sudo airmon-ng check kill")
+
+
+#restore wifi on exit
+@atexit.register
+def goodbye():
+	print("Goodbye!")
+	os.system("sudo service NetworkManager restart")
+
+
+#load environment vars
 load_dotenv()
 wlan_name=os.getenv('WLAN_NAME')
+
+
+#run aircrack in background
+def runInBackground():
+	os.system("sudo airmon-ng start "+wlan_name)
+
+p=multiprocessing.Process(target=runInBackground)
+p.start()
+
+
+#flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template("index.html")
 
-def runApp():
-	print('Running app in background')
-	app.run(port=8080)
+app.run(port=8080)
 
-if __name__ == '__main__':
-	p=multiprocessing.Process(target=runApp)
-	p.start()
 
-os.system("sudo airmon-ng start "+wlan_name)
+
