9bc671bb162f19ae08e397c1bf13233652e4de15
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2021-05-13 14:11:10 +0200

    interface not found error

--- server.py
+++ server.py
@@ -1,10 +1,32 @@
 from flask import Flask,render_template
 from dotenv import load_dotenv
-import os,multiprocessing,subprocess,atexit
+import os,multiprocessing,subprocess,atexit,psutil
 
 
+#check privs
 if os.geteuid() != 0:
 	print("This script requires sudo privs!")
+	exit()
+
+
+#load environment vars
+load_dotenv()
+wlan_name=os.getenv('WLAN_NAME')
+
+
+#check wifi interface
+addresses = psutil.net_if_addrs()
+stats = psutil.net_if_stats()
+
+available_networks = []
+for intface, addr_list in addresses.items():
+    if any(getattr(addr, 'address').startswith("169.254") for addr in addr_list):
+        continue
+    elif intface in stats and getattr(stats[intface], "isup"):
+        available_networks.append(intface)
+
+if not wlan_name in available_networks:
+	print(wlan_name+" interface not found!")
 	exit()
 
 
@@ -17,11 +39,6 @@ os.system("sudo airmon-ng check kill")
 def goodbye():
 	print("Goodbye!")
 	os.system("sudo service NetworkManager restart")
-
-
-#load environment vars
-load_dotenv()
-wlan_name=os.getenv('WLAN_NAME')
 
 
 #run aircrack in background
